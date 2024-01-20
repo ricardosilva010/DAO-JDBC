1b10820a1cce32a5760dd12e98c46025866d6f94
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-01-20 11:46:08 +0000

    SellerDaoJDBC and FactoryDao

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.FactoryDao;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Main
         Department department = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(1998, 1, 1), 3000.0, department);
+
+        SellerDao sellerDao = FactoryDao.createSellerDao();
 
         System.out.println(seller);
     }
