b07da7324d96da4ee3b6ce944494f40d03fc4150
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-01-21 12:48:00 +0000

    findByDepartment implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,9 +2,10 @@ package application;
 
 import model.dao.FactoryDao;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
-import java.time.LocalDate;
+import java.util.List;
 
 public class Main
 {
@@ -13,9 +14,15 @@ public class Main
         SellerDao sellerDao = FactoryDao.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
-
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> sellerList = sellerDao.findByDepartment(department);
+        for (Seller obj : sellerList)
+        {
+            System.out.println(obj);
+        }
     }
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -103,7 +103,8 @@ public class Seller implements Serializable
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Seller [id = " + id + ", name = " + name + ", email = " + email + ", birthDate = " + birthDate + ", baseSalary = " + baseSalary + ", department = " + department + "]";
     }
 }
