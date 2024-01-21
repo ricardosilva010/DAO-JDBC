5edeb0b3445132504967998b217933c48e05bc10
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-01-21 14:18:50 +0000

    update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -37,5 +37,11 @@ public class Main
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.of(2000, 1, 1), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -71,6 +71,32 @@ public class SellerDaoJDBC implements SellerDao
     @Override
     public void update(Seller seller)
     {
+        PreparedStatement preparedStatement = null;
+
+        try
+        {
+            preparedStatement = connection.prepareStatement(
+                "UPDATE seller " +
+                    "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " +
+                    "WHERE Id = ?");
+
+            preparedStatement.setString(1, seller.getName());
+            preparedStatement.setString(2, seller.getEmail());
+            preparedStatement.setDate(3, java.sql.Date.valueOf(seller.getBirthDate()));
+            preparedStatement.setDouble(4, seller.getBaseSalary());
+            preparedStatement.setInt(5, seller.getDepartment().getId());
+            preparedStatement.setInt(6, seller.getId());
+
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new DbException(e.getMessage());
+        }
+        finally
+        {
+            DB.closeStatement(preparedStatement);
+        }
     }
 
     @Override
